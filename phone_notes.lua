4ba4d4dc992ff0cc60d9f82203002007290f60c8
Ruqen <73693641+Ruqen@users.noreply.github.com>
2020-12-02 11:05:22 +0300

    Update phone_notes.lua

--- phone_notes.lua
+++ phone_notes.lua
@@ -1,7 +1,3 @@
-sv sideda registerservercallback yapılacak getInventoryItem(item).count
-
 mythic_sounds interactsound ile değiştirilecek
-
-client twitter.lua
 
 esx düzenleme triggerserverevent playerspawned
